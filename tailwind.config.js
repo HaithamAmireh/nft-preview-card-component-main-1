e2bea7e7cb76fed3144c50152db0cea7cb0fb0a4
Haitham Amireh <haithamamireh@gmail.com>
2021-11-20 14:37:15 +0200

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  purge: ["/nft-preview-card-component-main/dist/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      fontFamily:{
+        'main-font': ['Outfit', 'sans-serif'],
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
